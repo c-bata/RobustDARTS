8126087b50eaf3cec27d074926069083369e83ed
c-bata <contact@c-bata.link>
2019-11-20 17:53:51 +0900

    Migrate train to torch v1.0

--- src/evaluation/train.py
+++ src/evaluation/train.py
@@ -1,25 +1,19 @@
 import os
 import sys
-import time
-import glob
 import numpy as np
 import torch
 import json
 import codecs
 import yaml
 import logging
-import argparse
 import torch.nn as nn
 import torch.utils
-import torchvision.datasets as dset
 import torch.backends.cudnn as cudnn
-from torch.autograd import Variable
 from torch.optim.lr_scheduler import CosineAnnealingLR
 
 sys.path.insert(0, '../RobustDARTS')
 
 from src import utils
-from src.utils import Genotype
 from src.evaluation.model import Network
 from src.evaluation.args import Helper
 
@@ -37,21 +31,15 @@ fh = logging.FileHandler(os.path.join(args.save,
 fh.setFormatter(logging.Formatter(log_format))
 logging.getLogger().addHandler(fh)
 
-# add here torch version >= 1.0
-if TORCH_VERSION.startswith('1'):
-    device = torch.device('cuda:{}'.format(args.gpu))
+device = torch.device('cuda:{}'.format(args.gpu) if torch.cuda.is_available() else 'cpu')
+
 
 def main():
-  if not torch.cuda.is_available():
-    logging.info('no gpu device available')
-    sys.exit(1)
-
   np.random.seed(args.seed)
-  if TORCH_VERSION.startswith('1'):
-    torch.cuda.set_device(args.gpu)
+  torch.cuda.set_device(args.gpu)
   cudnn.benchmark = True
   torch.manual_seed(args.seed)
-  cudnn.enabled=True
+  cudnn.enabled = True
   torch.cuda.manual_seed(args.seed)
   logging.info('gpu device = %d' % args.gpu)
   logging.info("args = %s", args)
@@ -65,22 +53,14 @@ def main():
 
   print(arch)
   genotype = eval(arch)
-  model = Network(args.init_channels, args.n_classes, args.layers, args.auxiliary, genotype)
-  if TORCH_VERSION.startswith('1'):
-    model = model.to(device)
-  else:
-    model = model.cuda()
+  model = Network(args.init_channels, args.n_classes, args.layers, args.auxiliary, genotype).to(device)
 
   if args.model_path is not None:
     utils.load(model, args.model_path, genotype)
 
   logging.info("param size = %fMB", utils.count_parameters_in_MB(model))
 
-  criterion = nn.CrossEntropyLoss()
-  if TORCH_VERSION.startswith('1'):
-    criterion = criterion.to(device)
-  else:
-    criterion = criterion.cuda()
+  criterion = nn.CrossEntropyLoss().to(device)
 
   optimizer = torch.optim.SGD(
       model.parameters(),
@@ -123,44 +103,33 @@ def main():
 
   utils.write_yaml_results_eval(args, args.results_test, 100-valid_acc)
 
+
 def train(train_queue, model, criterion, optimizer):
   objs = utils.AvgrageMeter()
   top1 = utils.AvgrageMeter()
   top5 = utils.AvgrageMeter()
   model.train()
 
-  for step, (input, target) in enumerate(train_queue):
-    if TORCH_VERSION in ['1.0.1', '1.1.0']:
-      input = input.to(device)
-      target = target.to(device)
-    else:
-      input = Variable(input).cuda()
-      target = Variable(target).cuda(async=True)
+  for step, (input_tensor, target) in enumerate(train_queue):
+    input_tensor = input_tensor.to(device)
+    target = target.to(device)
 
     optimizer.zero_grad()
-    logits, logits_aux = model(input)
+    logits, logits_aux = model(input_tensor)
     loss = criterion(logits, target)
     if args.auxiliary:
       loss_aux = criterion(logits_aux, target)
       loss += args.auxiliary_weight*loss_aux
     loss.backward()
 
-    if TORCH_VERSION.startswith('1'):
-      nn.utils.clip_grad_norm_(model.parameters(), args.grad_clip)
-    else:
-      nn.utils.clip_grad_norm(model.parameters(), args.grad_clip)
+    nn.utils.clip_grad_norm_(model.parameters(), args.grad_clip)
     optimizer.step()
 
     prec1, prec5 = utils.accuracy(logits, target, topk=(1, 5))
-    n = input.size(0)
-    if TORCH_VERSION.startswith('1'):
-      objs.update(loss.item(), n)
-      top1.update(prec1.item(), n)
-      top5.update(prec5.item(), n)
-    else:
-      objs.update(loss.data[0], n)
-      top1.update(prec1.data[0], n)
-      top5.update(prec5.data[0], n)
+    n = input_tensor.size(0)
+    objs.update(loss.item(), n)
+    top1.update(prec1.item(), n)
+    top5.update(prec5.item(), n)
 
     if step % args.report_freq == 0:
       logging.info('train %03d %e %f %f', step, objs.avg, top1.avg, top5.avg)
@@ -176,44 +145,24 @@ def infer(valid_queue, model, criterion):
   top5 = utils.AvgrageMeter()
   model.eval()
 
-  if TORCH_VERSION.startswith('1'):
-    with torch.no_grad():
-      for step, (input, target) in enumerate(valid_queue):
-        input = input.to(device)
-        target = target.to(device)
+  with torch.no_grad():
+    for step, (input_tensor, target) in enumerate(valid_queue):
+      input_tensor = input_tensor.to(device)
+      target = target.to(device)
 
-        logits, _ = model(input)
-        loss = criterion(logits, target)
-
-        prec1, prec5 = utils.accuracy(logits, target, topk=(1, 5))
-        n = input.size(0)
-        objs.update(loss.item(), n)
-        top1.update(prec1.item(), n)
-        top5.update(prec5.item(), n)
-
-        if step % args.report_freq == 0:
-          logging.info('valid %03d %e %f %f', step, objs.avg, top1.avg, top5.avg)
-          if args.debug:
-            break
-  else:
-    for step, (input, target) in enumerate(valid_queue):
-      input = Variable(input, volatile=True).cuda()
-      target = Variable(target, volatile=True).cuda(async=True)
-
-      logits, _ = model(input)
+      logits, _ = model(input_tensor)
       loss = criterion(logits, target)
 
       prec1, prec5 = utils.accuracy(logits, target, topk=(1, 5))
-      n = input.size(0)
-      objs.update(loss.data[0], n)
-      top1.update(prec1.data[0], n)
-      top5.update(prec5.data[0], n)
+      n = input_tensor.size(0)
+      objs.update(loss.item(), n)
+      top1.update(prec1.item(), n)
+      top5.update(prec5.item(), n)
 
       if step % args.report_freq == 0:
         logging.info('valid %03d %e %f %f', step, objs.avg, top1.avg, top5.avg)
         if args.debug:
           break
-
   return top1.avg, objs.avg
 
 
